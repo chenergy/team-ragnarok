f0e860e3f9ac0f616428e5531101c3122afebf1c
Sothearith <sseng81@gmail.com>
2013-11-20 21:11:25 -0800

    Airship Level updated with textures. Will add animations later.

---
+++ Assets/Ship_UI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship_UI : MonoBehaviour {
+
+
+	public float Speed = 2;
+	
+	void Update () {
+	
+		transform.Rotate(Vector3.forward * Time.deltaTime * Speed);
+	}
+}
